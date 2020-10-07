5435d65a5f2e74280d4b7c85ba3c90eaea6add35
pauffret <pierreauffret@free.fr>
2020-10-07 11:04:57 +0200

    :)

---
+++ dalAccess.js
@@ -0,0 +1,24 @@
+var dalAccess = require('dalAccess');
+
+//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+var options = {
+    host: 'http://localhost/',
+    //TODO
+    path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+};
+
+callback = function(response) {
+    var str = '';
+
+    //another chunk of data has been received, so append it to `str`
+    response.on('data', function (chunk) {
+        str += chunk;
+    });
+
+    //the whole response has been received, so we just print it out here
+    response.on('end', function () {
+        console.log(str);
+    });
+}
+
+dalAccess.request(options, callback).end();
