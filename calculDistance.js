f3b10cbfa288c6c289a3a8902433851d6513497b
pauffret <pierreauffret@free.fr>
2020-10-07 11:07:08 +0200

    :)

--- calculDistance.js
+++ calculDistance.js
@@ -3,7 +3,7 @@ require('https://maps.googleapis.com/maps/api/js?key=AIzaSyDatb-OuXM3HnUrSN3LBEi
 var p1 = new google.maps.LatLng(45.463688, 9.18814);
 var p2 = new google.maps.LatLng(46.0438317, 9.75936230000002);
 
-function calculateDistance(p1, p2) {
+function calculateDistance(p1,p2) {
     return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
 }
 
